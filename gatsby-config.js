eb347a31c97214772461b5bdd9a49cabfeb89b1b
Tam <studio@tamlove.com>
2022-07-26 17:38:16 +0100

    Added font, icon, bootstrap and waypoint packages.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,45 @@
 module.exports = {
   siteMetadata: {
-    title: `Osiris`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `Osiris Resources | Data Driven Recruitment`,
+    shortTitle: `Osiris Resources`,
+    siteUrl: `https://osirisresources.com`,
+    titleTemplate: `%s | Osiris Resources`,
+    socials: [
+      {
+        url: `https://facebook.com`,
+      },
+      {
+        url: `https://www.linkedin.com/company/osiris-resources-consultancy/`,
+      },
+      {
+        url: `https://twitter.com`,
+      },
+    ]
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
-    },
-    __key: "images"
-  }]
+  plugins: [ 
+            "gatsby-plugin-react-helmet", 
+            "gatsby-plugin-sitemap", 
+            "gatsby-plugin-image",
+            "gatsby-plugin-sharp", 
+            "gatsby-transformer-sharp", 
+            {
+                resolve: 'gatsby-source-filesystem',
+                options: {
+                  "name": "images",
+                  "path": "./src/images/"
+                },
+                __key: "images"
+            },
+            {
+              resolve: `gatsby-plugin-google-fonts`,
+              options: {
+                fonts: [
+                  `cabin\:400,600,700`,
+                  `dosis\:400,600`
+                ],
+                display: 'swap'
+              }
+            },
+            "gatsby-plugin-fontawesome-css",
+        ]
 };
