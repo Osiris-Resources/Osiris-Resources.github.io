b6edc9d876ed9aaa046a052189b73eb2dafc3557
Tam <studio@tamlove.com>
2022-07-29 10:43:48 +0100

    Styled Hero section text and buttons.

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,17 +11,17 @@ const IndexPage = () => {
                 <div className="hero-container-sub">
                     <div className="container">
                         <div className="row">
-                            <div className="col-lg-5">
+                            <div className="col-lg-6">
                                 <div className="hero-content">
                                     <h1>Data Driven Recruitment</h1>
-                                    <h3 className="dark-text">Osiris Resources is an expert consultancy delivering global hiring programs for clients in the Telecommunication and Computer Networking space.</h3>
+                                    <h3>Osiris Resources is an <span>expert consultancy delivering global hiring programs</span> for clients in the <span>Telecommunication</span> and <span>Computer Networking</span> space.</h3>
                                     <div className="hero-cta">
                                         <a className="btn" href="#">Find Out More</a>
                                         <a className="btn hero-btn-last" href="">Register Interest</a>
                                     </div>
                                 </div>
                             </div>
-                            <div className="col-lg-7">
+                            <div className="col-lg-6">
                                 <div className="hero-image">
                                     <div className="hero-image-container">
                                     </div>
