24ea4933e7b6549e70f00715b90efd0da6beb7d1
Tam <studio@tamlove.com>
2022-08-01 15:21:41 +0100

    Added animation to About header.

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,12 @@ const scrollTo = (e, sectionName) => {
     const section = document.getElementById(`${sectionName}`)
     e.preventDefault()
     section && section.scrollIntoView({ behavior: "smooth", block: "start" })
-  }
+}
+
+const highlightAnimation = (isActiveSection) => {
+    const highlight = document.getElementById("highlight")
+    isActiveSection ? highlight.classList.add("animate-highlight") : highlight.classList.remove("animate-highlight")
+}
 
 const IndexPage = () => {
   const [isHero, setIsHero] = useState(true)
@@ -247,19 +252,21 @@ const IndexPage = () => {
             </div>
         </div>
         <div id="about" className="about-container">
-            <div className="about-info">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-12 text-center">
-                            <h2 className="highlight-heading">About Us</h2>
-                            <p className="text-accent">Osiris is an industry specific recruitment consultancy that supports and delivers high level recruitment partnership programmes and third party assistance. We have spent multiple years focusing solely on roles within the Telecoms / Computer Networking Industry.</p>
-                            <hr />
-                            <p>What does industry specific mean? SDN / NFV, Containerisation OS systems / Virtualization, Edge Computing / Cloud Computing (Platforms with API capability) and 5G / mmWave / RAN / Backhaul technology.
-                                We understand our technology space, the demographic of talent within our market and regularly deliver in what can be a challenging space to hire effectively and quickly.  We are confident we can drive the CAPEX and OPEX of your hiring programs down.</p>
+            <Waypoint onEnter={() => highlightAnimation(true)} onLeave={() => highlightAnimation(false)}>
+                <div className="about-info">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-12 text-center">
+                            <h2 className="highlight-heading"><span id="highlight"></span>About Us</h2>
+                                <p className="text-accent">Osiris is an industry specific recruitment consultancy that supports and delivers high level recruitment partnership programmes and third party assistance. We have spent multiple years focusing solely on roles within the Telecoms / Computer Networking Industry.</p>
+                                <hr className="divider-small"></hr>
+                                <p>What does industry specific mean? SDN / NFV, Containerisation OS systems / Virtualization, Edge Computing / Cloud Computing (Platforms with API capability) and 5G / mmWave / RAN / Backhaul technology.
+                                    We understand our technology space, the demographic of talent within our market and regularly deliver in what can be a challenging space to hire effectively and quickly.  We are confident we can drive the CAPEX and OPEX of your hiring programs down.</p>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </Waypoint>
         </div>
     </Layout>
   )
