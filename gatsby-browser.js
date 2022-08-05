e1b2d874da4ada8f4a3bc10786bd2251e0d193d0
Tam <studio@tamlove.com>
2022-08-05 15:55:36 +0100

    Added main style to Services section.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import "swiper/css";
 import "swiper/css/pagination";
-import "swiper/css/navigation";
 import "./src/styles/normalize.css"
 import "./src/styles/global.css"
--- src/components/services-swiper.js
+++ src/components/services-swiper.js
@@ -1,6 +1,16 @@
 import * as React from 'react'
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination } from "swiper";
+import { Autoplay, Pagination } from "swiper";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+    faCheck,
+    faGlobe,
+    faBuilding,
+    faMoneyCheckDollar,
+    faMicrochip,
+    faPersonCircleCheck,
+    faFlagUsa,
+  } from '@fortawesome/free-solid-svg-icons'
 
 const pagination = {
     clickable: true,
@@ -12,27 +22,33 @@ const pagination = {
 const ServicesSwiper = () => {
     return(
         <>
-            <Swiper navigation pagination={pagination} modules={[Navigation, Pagination]} className="services-swiper">
+            <Swiper autoplay={{
+                            delay: 7500,
+                            disableOnInteraction: true,
+                        }} 
+                    pagination={pagination} 
+                    modules={[Autoplay, Pagination]} 
+                    className="services-swiper">
                 <SwiperSlide>
                     <div className="services-swiper-slide">
-                        <p><span></span>Data driven recruitment</p>
-                        <p><span></span>Monthly contracted hiring resources</p>
-                        <p><span></span>Pipeline building and account management services</p>
-                        <p><span></span>Successful LinkedIn campaigns</p>
+                        <p><span><FontAwesomeIcon icon={faCheck} /></span>Data driven recruitment</p>
+                        <p><span><FontAwesomeIcon icon={faCheck} /></span>Monthly contracted hiring resources</p>
+                        <p><span><FontAwesomeIcon icon={faCheck} /></span>Pipeline building and account management services</p>
+                        <p><span><FontAwesomeIcon icon={faCheck} /></span>Successful LinkedIn campaigns</p>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide>
                     <div className="services-swiper-slide">
-                        <p><span></span>2019: 38 staff hired across 4 locations in 12 months - USA, Spain, Japan and UK</p>
-                        <p><span></span>Company entities created in the UK and an office in Madrid created in that time</p>
-                        <p><span></span>Company is now formerly known as IBM after acquisition</p>
+                        <p><span><FontAwesomeIcon icon={faGlobe} /></span>2019: 38 staff hired across 4 locations in 12 months - USA, Spain, Japan and UK</p>
+                        <p><span><FontAwesomeIcon icon={faBuilding} /></span>Company entities created in the UK and an office in Madrid created in that time</p>
+                        <p><span><FontAwesomeIcon icon={faMoneyCheckDollar} /></span>Company is now formerly known as IBM after acquisition</p>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide>
                     <div className="services-swiper-slide">
-                        <p><span></span>2022: 20 requirements to build a EMEA Engineering team in Spain and sales organisation in the USA</p>
-                        <p><span></span>On track to deliver 15 headcount in the engineering team by late 2022</p>
-                        <p><span></span>Building a product development team in the USA</p>
+                        <p><span><FontAwesomeIcon icon={faMicrochip} /></span>2022: 20 requirements to build a EMEA Engineering team in Spain and sales organisation in the USA</p>
+                        <p><span><FontAwesomeIcon icon={faPersonCircleCheck} /></span>On track to deliver 15 headcount in the engineering team by late 2022</p>
+                        <p><span><FontAwesomeIcon icon={faFlagUsa} /></span>Building a product development team in the USA</p>
                     </div>
                 </SwiperSlide>
             </Swiper>
