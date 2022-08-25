685f6cc4ce3819e6084a60b2d2cf43c9b1f98d6d
Tam <studio@tamlove.com>
2022-08-25 15:33:57 +0100

    Added Privacy Policy.

---
+++ src/pages/privacy.js
@@ -0,0 +1,77 @@
+import * as React from "react"
+import Layout from "../components/layout"
+
+const Privacy = () => {
+    return (
+        <Layout isArticle={false} isHero={false} simpleNav={true}>
+            <div className="container mt-5 mb-5 privacy-container">
+                <div className="row mt-5 mb-5">
+                    <div className="col-12 mt-5 mb-5">
+                        <h1 className="text-center mt-5">Privacy Policy</h1>
+                        <br/>
+                        <h2 className="mt-5 mb-3">Our contact details</h2>
+                        <p>James Hill (Director), 128 City Road, London, United Kingdom, EC1V 2NX</p>
+                        <p><a href="tel:+441179410523">+44 (0) 117 941 0523</a></p>
+                        <p><a href="mailto:info@osirisresources.com">info[@]osirisresources.com</a></p>
+                        <br/>
+                        <br/>
+                        <h2 className="mt-4 mb-3">The type of personal information we collect</h2>
+                        <p className="bold-text">We currently collect and process the following information:</p>
+                        <p><span className="bold-text">Candidates:</span> When you register with us as a candidate to provide work finding services to you, you provide and we process your personal data, including but not limited to your name, contact details and information from your CV, current benefits and your expectations for future roles. Where permitted by law, we may also collect information relating to your health (for example, disability information for reasonable work adjustment purposes), diversity information, including race, ethnic origin, sexual orientation and religion (for equal opportunity monitoring purposes) as well as details of any unspent criminal convictions where required by a client or by us if you apply for a role with us.</p>
+                        <p><span className="bold-text">Prospective Candidates:</span> We collect personal data about you, including your name and contact details and professional biographical details obtained from publicly available sources such as LinkedIn and social media sites, so that we can contact you if we think you may be interested in our work finding services at a future date. We may also obtain your personal data through another candidate or an employer who recommends you as a contact.</p>
+                        <p><span className="bold-text">Users of our website or apps:</span> We collect personal data such as your IP address and other data about your device which we need to provide our online content to you. We also collect data about your engagement with our website or apps such as the pages you view. If you contact us, we will also collect information about your enquiry.</p>
+                        <p><span className="bold-text">Prospective Business clients:</span> We collect your contact details and data relating to your function or position to establish commercial relationships with your company.</p>
+                        <p><span className="bold-text">Clients:</span> We collect personal data such as your name, job title and contact details. We also process personal data about what communications we have had with you, including whether you have opened or forwarded any emails, newsletters or other content we have sent to you. We also process feedback that you provide about our candidates.</p>
+                        <p><span className="bold-text">Suppliers:</span> We typically collect your name and contact details as a business contact for your organisation.</p>
+                        <br/>
+                        <h2 className="mt-4 mb-3">How we get the personal information and why we have it</h2>
+                        <p className="bold-text">Most of the personal information we process is provided to us directly by you for one of the following reasons:</p>
+                        <ul>
+                            <li>Provide you with work finding services;</li>
+                            <li>Communicate with you;</li>
+                            <li>Enable you to upload your CV and apply for jobs;</li>
+                            <li>Personalise your experience by creating a candidate profile;</li>
+                            <li>Enable us to monitor equality and diversity;</li>
+                            <li>Provide you with interviewing and salary advice;</li>
+                            <li>Send you direct marketing for the purposes of informing you about job opportunities, industry reports and insights, events, promotions and competitions, and other content in accordance with your marketing choices;</li>
+                        </ul>
+                        <p className="bold-text">Under the UK General Data Protection Regulation (UK GDPR), the lawful bases we rely on for processing this information are: </p>
+                        <p><span className="bold-text">Your consent.</span> You are able to remove your consent at any time. You can do this by contacting us.</p>
+                        <br/>
+                        <h2 className="mt-4 mb-3">How we store your personal information</h2>
+                        <p>Your information is securely stored. We have implemented and maintained appropriate technical and organisational measures to protect your personal data from accidental or unlawful destruction, loss, alteration, unauthorised disclosure or access. Only authorised personnel and third-party service providers are provided with access to personal data on a need-to-know basis, and these employees and service providers are contractually obliged to treat this information as confidential.</p>
+                        <br/>
+                        <h2 className="mt-4 mb-3">How long we hold your personal data for</h2>
+                        <p>We will keep your personal data for as long as we need to in order to fulfil the purpose we collected it for, which may be an ongoing purpose. For example, if you’re a candidate, we will retain your personal data for the duration of our business relationship with you and beyond, as we often support candidates with job placements over many years and potentially throughout their careers.</p>
+                        <p>We keep some personal data for longer than others. To determine the appropriate retention period for personal data, we consider factors such as the purposes for which we process your personal data, including any legal, regulatory, accounting and reporting obligations, the nature and amount of personal data that we hold about you, and the potential risk of harm to you from unauthorised use or disclosure of your personal data.</p>
+                        <p>Where we process your personal data for direct marketing purposes, we will do so until you ask us to stop, and for a short period after this (to allow us to implement your request). We also keep a record of the fact that you have asked us not to send you direct marketing or to process your data indefinitely, so that we can respect your request.</p>
+                        <br/>
+                        <h2 className="mt-4 mb-3">Your data protection rights</h2>
+                        <p className="bold-text">Under data protection law, you have rights including:</p>
+                        <p><span className="bold-text">Your right of access</span> - You have the right to ask us for copies of your personal information.</p>
+                        <p><span className="bold-text">Your right to rectification</span> - You have the right to ask us to rectify personal information you think is inaccurate. You also have the right to ask us to complete information you think is incomplete.</p>
+                        <p><span className="bold-text">Your right to erasure</span> - You have the right to ask us to erase your personal information in certain circumstances.</p>
+                        <p><span className="bold-text">Your right to restriction of processing</span> - You have the right to ask us to restrict the processing of your personal information in certain circumstances.</p>
+                        <p><span className="bold-text">Your right to object to processing</span> - You have the right to object to the processing of your personal information in certain circumstances.</p>
+                        <p><span className="bold-text">Your right to data portability</span> - You have the right to ask that we transfer the personal information you gave us to another organisation, or to you, in certain circumstances.</p>
+                        <p>You are not required to pay any charge for exercising your rights. If you make a request, we have one month to respond to you. Please contact us if you wish to make a request.</p>
+                        <br/>
+                        <h2 className="mt-4 mb-3">How to complain</h2>
+                        <p className="bold-text">If you have any concerns about our use of your personal information, you can make a complaint to us at:</p>
+                        <p>James Hill (Director), 128 City Road, London, United Kingdom, EC1V 2NX</p>
+                        <p><a href="tel:+441179410523">+44 (0) 117 941 0523</a></p>
+                        <p><a href="mailto:info@osirisresources.com">info[@]osirisresources.com</a></p>
+                        <br/>
+                        <br/>
+                        <p>You can also complain to the ICO if you are unhappy with how we have used your data.</p>
+                        <p>Information Commissioner’s Office, Wycliffe House, Water Lane, Wilmslow, Cheshire, SK9 5AF</p>
+                        <p><a href="tel:+443031231113">+44 (0) 303 123 1113</a></p>
+                        <p>Alternatively you can visit the <a href="https://www.ico.org.uk" target="_blank" rel="noreferrer">ICO website</a> for more information.</p>
+                    </div>
+                </div>
+            </div>
+        </Layout>
+    )
+}
+
+export default Privacy
